ae088a633482fb78d259c5504abfc81772e99723
Justin D. Fetherolf <fetherolfjd@gmail.com>
2021-12-19 21:58:08 -0700

    Fix start
    Wait for process to die, if killing.
    Background new server process.

--- start.sh
+++ start.sh
@@ -8,7 +8,15 @@ mc_svr_pid=$(ps -ef | grep minecraft-server.jar | grep -v grep | awk -F' ' '{pri
 if [ -n "$mc_svr_pid" ]; then
   echo "Stopping minecraft server process: $mc_svr_pid"
   kill "$mc_svr_pid"
+  killed="false"
+  while [ "$killed" = "false" ]; do
+    killed_pid=$(ps -ef | grep minecraft-server.jar | grep -v grep | awk -F' ' '{print $2}')
+    if [ -z "$killed_pid" ]; then
+      echo "Minecraft process '$mc_svr_pid' has been killed."
+      killed="true"
+    fi
+  done
 fi
 
 pushd "${mc_home}/current"
-java -Xms1G -Xmx1G -jar "$jar_name" --nogui
+java -Xms1G -Xmx1G -jar "$jar_name" --nogui &
